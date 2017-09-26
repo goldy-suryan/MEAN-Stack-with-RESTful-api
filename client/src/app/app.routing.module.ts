54fc92ffe8e1ffaa4d5e358a6647f25feefd790a
Goldy <goldysuryan87@gmail.com>
2017-09-26 16:41:02 +0530

    added routes for login

--- client/src/app/app.routing.module.ts
+++ client/src/app/app.routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 import { HomeComponent } from "./home/home.component";
 import { NotFoundComponent } from "./notfound/notfound.component";
 import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from './login/login.component';
 import { AuthService } from "./auth.service";
 
 const routes: Routes = [
+    { path: "", redirectTo: "/login", pathMatch: "full"},
     { path: "home", component: HomeComponent },
-    { path: "", redirectTo: "/home", pathMatch: "full"},
     { path: "signup", component: SignupComponent },
+    { path: "login", component: LoginComponent },
     { path: "**", component: NotFoundComponent }
 ];
 
@@ -19,11 +22,13 @@ const routes: Routes = [
     declarations: [
         HomeComponent,
         NotFoundComponent,
-        SignupComponent
+        SignupComponent,
+        LoginComponent
     ],
     imports: [ 
         FormsModule,
         HttpModule,
+        CommonModule,
         RouterModule.forRoot(routes) 
     ],
     exports: [ RouterModule ],
