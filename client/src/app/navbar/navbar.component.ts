52f14dd362fb36c3654e9839734a2e2272fd0010
Goldy <goldysuryan87@gmail.com>
2017-09-27 15:32:18 +0530

    change detection

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
+  }
+
+  loggingSession() {
+    if(typeof Storage !== undefined) {
+      if(sessionStorage.getItem("user")) {
+        return true;
+      } 
+      return false;
+    }
   }
 
 }
