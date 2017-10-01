58e1425721b71056fa6f29838bbea9ebf8cf9f48
Goldy <goldysuryan87@gmail.com>
2017-10-01 16:42:12 +0530

    added blog routes

--- client/src/app/app.routing.module.ts
+++ client/src/app/app.routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HttpModule } from "@angular/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from  "@angular/forms";
 import { CommonModule } from "@angular/common";
 
 import { HomeComponent } from "./home/home.component";
@@ -9,15 +9,17 @@ import { NotFoundComponent } from "./notfound/notfound.component";
 import { SignupComponent } from "./signup/signup.component";
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { AuthService } from "./auth.service";
-import { AuthGuard} from "./auth-guard.service";
+import { BlogsComponent } from './blog/blog.component';
+import { AuthGuard } from "./auth-guard.service";
+import { BlogServiceResolve } from "./blog/blog.service";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/login", pathMatch: "full"},
+    { path: "", redirectTo: "/login", pathMatch: "full" },
     { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
     { path: "signup", component: SignupComponent },
     { path: "login", component: LoginComponent },
     { path: "logout", component: LogoutComponent },
+    { path: "blogs", component: BlogsComponent, resolve: { blogs: BlogServiceResolve } },
     { path: "**", component: NotFoundComponent }
 ];
 
@@ -27,19 +29,19 @@ const routes: Routes = [
         NotFoundComponent,
         SignupComponent,
         LoginComponent,
-        LogoutComponent
+        LogoutComponent,
+        BlogsComponent
     ],
-    imports: [ 
-        FormsModule,
+    imports: [
         HttpModule,
         CommonModule,
-        RouterModule.forRoot(routes) 
+        FormsModule,
+        RouterModule.forRoot(routes)
     ],
-    exports: [ RouterModule ],
+    exports: [RouterModule],
     providers: [
-        AuthService,
         AuthGuard
     ]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
