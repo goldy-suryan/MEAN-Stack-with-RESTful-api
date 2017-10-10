101bd2d9ed7d9e4a885d3380a64672d60ef76262
Goldy <goldysuryan87@gmail.com>
2017-10-11 01:17:53 +0530

    signup route simplified

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,6 @@ let Strategy = require("passport-local").Strategy;
 let mongoose = require("mongoose");
 const User = require("../model/usermodel").userModel;
 let bcrypt = require("bcrypt");
-let saltRounds = 10;
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -15,38 +14,6 @@ passport.deserializeUser((id, done) => {
         done(err, user);
     });
 });
-
-
-passport.use("local.signup", new Strategy({
-    usernameField: "username",
-    passwordField: "password",
-    passReqToCallback: true
-}, (req, username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-        if (err) {
-            if (err.code === 11000) {
-                return done(err);
-            }
-            return done(err);
-        }
-
-        if (user) { return done(null, false) }
-
-        if (!user) {
-            bcrypt.hash(password, saltRounds, (err, hash) => {
-                var newUser = new User();
-                newUser.username = req.body.username;
-                newUser.email = req.body.email;
-                newUser.password = hash;
-
-                newUser.save((err) => {
-                    if (err) { return done(err) }
-                    done(null, newUser);
-                });
-            });
-        }
-    });
-}));
 
 passport.use("local.login", new Strategy({
     usernameField: "username",
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,22 +1,45 @@
 let express = require("express");
 let route = express.Router();
 let passport = require("passport");
+const User = require("../model/usermodel").userModel;
 let bcrypt = require("bcrypt");
 let saltRounds = 10;
-const User = require("../model/usermodel").userModel;
 
+route.post("/signup", (req, res, next) => {
+    User.findOne({ $or: [{ username: req.body.username}, { email: req.body.email }] }, (err, user) => {
 
-route.post("/signup", passport.authenticate("local.signup"), (req, res) => {
-    res.json({ success: true, message: "user saved" });
-});
+        if(err) {
+            return res.json(err);
+        }
 
-route.post("/login", passport.authenticate("local.login"), (req, res) => {
-    let data = {
-        username : req.user.username,
-        email: req.user.email,
-        id: req.user._id
-    }
-    res.json({ success: true, message: "logged in Successfully", data: data });
+        if (user) { 
+            return res.json({ success: false, message: "user already exists" }) }
+
+        if (!user) {
+            bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                var newUser = new User();
+                newUser.username = req.body.username;
+                newUser.email = req.body.email;
+                newUser.password = hash;
+
+                newUser.save((err) => {
+                    if (err) { return next(err) }
+                    res.json({ success: true, message: "user saved successfully" });
+                });
+            });
+        }
+    });
 })
+
+route.post("/login", (req, res, next) => {
+
+    passport.authenticate("local.login", (err, user, info) => {
+        if (err) { return next(err) }
+
+        if (!user) { return res.json({ success: false, message: "Username or Password doesn't match" }) }
+
+        res.json({ success: true, message: "logged in Successfully", data: user });
+    })(req, res, next);
+});
 
 module.exports = route;
