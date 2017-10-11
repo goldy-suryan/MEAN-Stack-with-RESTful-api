7456e13cf90c4a4f9d0f562d1877e07ee32b3f5e
Goldy <goldysuryan87@gmail.com>
2017-10-11 11:29:04 +0530

    form builder

--- client/src/app/blogs/blogs.component.ts
+++ client/src/app/blogs/blogs.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { Iblogs } from "../interfaces";
 import { AuthService } from "../auth.service";
+import { Subscription } from "rxjs/Subscription";
+import { SharedService } from '../shared.service';
+
 declare let $: any;
 
 @Component({
@@ -9,17 +13,36 @@ declare let $: any;
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css']
 })
-export class BlogsComponent implements OnInit {
 
+export class BlogsComponent implements OnInit {
+  blogForm: FormGroup;
   @ViewChild('myModal') myModal: ElementRef;
   blogs: any;
+  user;
+  subscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private service: AuthService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private service: AuthService, private sharedService: SharedService, private router: Router, private fb: FormBuilder, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    // Resolver to fetch the data
     this.route.data.subscribe((blogs) => {
       this.blogs = blogs.blogs;
     });
+
+    // creating the instance of formGroup with formBuilder
+    this.blogForm = this.fb.group({
+      title: '',
+      description: '',
+      createdBy: this.user
+    })
+
+    // getting the user from shared service
+    this.subscription = this.sharedService.getting().subscribe(
+      (user) => {
+        if (user == null) return;
+        this.user = user.data.username;
+      }
+    )
   }
 
   renavigate() {
@@ -33,8 +56,17 @@ export class BlogsComponent implements OnInit {
     }
   }
 
-  addBlog(val) {
-    this.service.newBlog(val).subscribe(
+  addBlog() {
+    let title = this.blogForm.get('title').value;
+    let description = this.blogForm.get('description').value;
+    let createdBy = this.blogForm.get('createdBy').value;
+    let blog = {
+      title: title,
+      description: description,
+      createdBy: createdBy
+    }
+
+    this.service.newBlog(blog).subscribe(
       (blog) => this.renavigate(),
       (err) => err
     );
@@ -44,10 +76,8 @@ export class BlogsComponent implements OnInit {
   deleteBlog(id) {
     this.service.deleteblog(id).subscribe(
       (blog) => this.renavigate(),
-      (err) => {
-        err
-      })
-
+      (err) => err
+    )
   }
 
   close() {
