3c66b7fc1c2d8e55dd413a5aea2171a5267a938c
Goldy <goldysuryan87@gmail.com>
2017-10-11 17:09:28 +0530

    login & signup

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,3 +1,4 @@
+import { DataService } from './data.service';
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
@@ -7,44 +8,20 @@ import { Observable } from "rxjs/Observable";
 import { ISignup, Ilogin, Iblogs } from "./interfaces";
 
 @Injectable()
-export class AuthService {
+export class AuthService extends DataService{
 
   signUpUrl: string = "http://localhost:3001/auth/signup";
   loginUrl: string = "http://localhost:3001/auth/login";
-  blogsUrl: string = "http://localhost:3001/allblogs/";
 
-  constructor( private http: Http ) { }
+  constructor( http: Http ) {
+    super("http://localhost:3001/allblogs/", http)
+   }
 
   signUp(data): Observable<any> {
-    return this.http.post(this.signUpUrl, data).map((res) => res.json()).catch(this.errorHandler);
+    return this.http.post(this.signUpUrl, data).map((res) => res.json());
   }
 
   login(user): Observable<any> {
-    return this.http.post(this.loginUrl, user).map((res) => res.json()).catch(this.errorHandler);
+    return this.http.post(this.loginUrl, user).map((res) => res.json());
   }
-
-  getBlogs(): Observable<Iblogs[]> {
-    return this.http.get(this.blogsUrl).map((blogs) => blogs.json()).catch(this.errorHandler);
-  }
-
-  getBlog(id): Observable<Iblogs> {
-    return this.http.get(this.blogsUrl + id).map((blog) => blog.json()).catch(this.errorHandler);
-  }
-
-  updateBlog(id, val): Observable<Iblogs> {
-    return this.http.put(this.blogsUrl+ id, val).map((blog) => blog.json()).catch(this.errorHandler);
-  }
-
-  newBlog(blog): Observable<Iblogs> {
-    return this.http.post(this.blogsUrl, blog).map((blog) => blog.json()).catch(this.errorHandler);
-  }
-
-  deleteblog(id): Observable<Iblogs> {
-    return this.http.delete(this.blogsUrl + id).map((blog)=> blog.json()).catch(this.errorHandler);
-  }
-
-  private errorHandler(err: Response ) {
-    return Observable.throw(err);
-  }
-
 }
