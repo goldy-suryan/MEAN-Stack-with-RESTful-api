63157e1872849ab254abf8bdc892363288b51b49
Goldy <goldysuryan87@gmail.com>
2017-10-13 17:07:49 +0530

    modified according to jwt

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { ToastrService } from 'toastr-ng2';
 import { BadRequestError } from './../Errors/badRequestError';
 import { AppError } from './../Errors/app.error';
-import { SharedService } from './../shared.service';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-login',
@@ -14,7 +14,7 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   user: any;
   error: any;
-  constructor(private authService: AuthService, private router: Router, private cdr: ChangeDetectorRef, private sharedService: SharedService, private toastrService: ToastrService) { }
+  constructor(private authService: AuthService, private router: Router, private cdr: ChangeDetectorRef, private toastrService: ToastrService) { }
 
   ngOnInit() {
   }
@@ -23,15 +23,15 @@ export class LoginComponent implements OnInit {
     this.authService.login(user).subscribe((user) => {
       this.user = user;
       this.cdr.detectChanges();  // detecting for changes in the component
-      if(typeof Storage !== undefined && user.success) {
-        sessionStorage.setItem("user", user.data._id); // storing the user ID to browser storage for simple login
-        this.sharedService.send(this.user); // sending the user data along, so as to used by other components too
+      if (typeof Storage !== undefined && user.success) {
+        sessionStorage.setItem("user", user.token); // storing the user ID to browser storage for simple login
         this.router.navigate(['/home']);
+        return true;
       }
     }, (err: AppError) => {
-          if(err instanceof BadRequestError) 
-            this.toastrService.error("Bad Request", "Error");
-          this.toastrService.error("An unexpected error occured", "Error")
+      if (err instanceof BadRequestError)
+        this.toastrService.error("Bad Request", "Error");
+      this.toastrService.error("An unexpected error occured", "Error")
     });
   }
 }
