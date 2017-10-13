ca46ebd0d3b53611eae5f108574fb7f252df411c
Goldy <goldysuryan87@gmail.com>
2017-10-13 17:04:28 +0530

    angular2-jwt

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,3 +1,5 @@
+import { JwtHelper } from 'angular2-jwt';
+import { Router } from '@angular/router';
 import { DataService } from './data.service';
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
@@ -13,7 +15,7 @@ export class AuthService extends DataService{
   signUpUrl: string = "http://localhost:3001/auth/signup";
   loginUrl: string = "http://localhost:3001/auth/login";
 
-  constructor( http: Http ) {
+  constructor( http: Http, private router: Router ) {
     super("http://localhost:3001/allblogs/", http)
    }
 
@@ -23,5 +25,17 @@ export class AuthService extends DataService{
 
   login(user): Observable<any> {
     return this.http.post(this.loginUrl, user).map((res) => res.json()).catch(this.errorHandler);
+  }
+
+  logOut() {
+    sessionStorage.removeItem("user");
+    this.router.navigate(["/login"]);
+  }
+
+  get CurrentUser() {
+    let token = sessionStorage.getItem("user");
+    if(!token) return null;
+
+    return new JwtHelper().decodeToken(token);
   }
 }
