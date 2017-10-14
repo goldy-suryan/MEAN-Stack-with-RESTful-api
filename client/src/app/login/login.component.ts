a81ab066bcee792254170c4e939adf4e211a3609
Goldy <goldysuryan87@gmail.com>
2017-10-14 16:13:52 +0530

    sending user to the required url

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { BadRequestError } from './../Errors/badRequestError';
 import { AppError } from './../Errors/app.error';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from "../auth.service";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 
 @Component({
@@ -14,24 +14,32 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   user: any;
   error: any;
-  constructor(private authService: AuthService, private router: Router, private cdr: ChangeDetectorRef, private toastrService: ToastrService) { }
+  constructor(private authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private toastrService: ToastrService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
   login(user) {
-    this.authService.login(user).subscribe((user) => {
-      this.user = user;
-      this.cdr.detectChanges();  // detecting for changes in the component
-      if (typeof Storage !== undefined && user.success) {
-        sessionStorage.setItem("user", user.token); // storing the user ID to browser storage for simple login
-        this.router.navigate(['/home']);
-        return true;
-      }
-    }, (err: AppError) => {
-      if (err instanceof BadRequestError)
-        this.toastrService.error("Bad Request", "Error");
-      this.toastrService.error("An unexpected error occured", "Error")
-    });
+    this.authService.login(user).subscribe(
+      (user) => {
+        this.user = user;
+        this.cdr.detectChanges();  // detecting for changes in the component
+
+        if (typeof Storage !== undefined && user.success) {
+          let returnUrl = this.route.snapshot.queryParamMap.get("returnUrl");
+          sessionStorage.setItem("user", user.token); // storing the user ID to browser storage for simple login
+          this.router.navigate([returnUrl || '/home']);
+          return true;
+        }
+      },
+      (err: AppError) => {
+        if (err instanceof BadRequestError)
+          this.toastrService.error("Bad Request", "Error");
+        this.toastrService.error("An unexpected error occured", "Error")
+      });
   }
 }
