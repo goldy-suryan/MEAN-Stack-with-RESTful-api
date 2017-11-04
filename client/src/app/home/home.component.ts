3ebeffbef4cd9cd359614f9b6de8c9c8a7604b3a
Goldy <goldysuryan87@gmail.com>
2017-11-04 17:22:40 +0530

    subjects added

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   username: string;
+  subjects = [ "mongoDB", "Express", "Angular 2 (4)", "Nodejs", "lorem ipsum", "lorem ipsum" ];
 
   constructor(private authService: AuthService) { }
 
